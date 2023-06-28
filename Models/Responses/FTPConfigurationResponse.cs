bdf751121bcf237f2cdfa318d5fc2a22da5d7609
Mario  hurtado <mhurtado@soaint.com>
2023-06-28 08:24:45 -0500

    inicio

---
+++ Models/Responses/FTPConfigurationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Directions_Api.Models.Responses
+{
+    public class FTPConfigurationResponse
+    {
+        public string Llave { get; set; }
+        public string Valor { get; set; }
+    }
+}
